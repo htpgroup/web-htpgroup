8e962e696331e0a6703eb220f55b14044b719c0e
dungpxhtp <96401256+dungpxhtp@users.noreply.github.com>
2022-06-10 09:58:29 +0700

    Add File DEPLOYMET.md

---
+++ app/Helpers/DataHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use App\Dictionaries\AppDomain;
+
+class DataHelper
+{
+    public static function getListDomainByApp($domain_id = AppDomain::FINDER)
+    {
+        $arrDomain = AppDomain::all();
+        switch ($domain_id) {
+            case AppDomain::FINDER:
+                $arrDomain = AppDomain::finderDomain();
+                break;
+            case AppDomain::SPACE_GOLF:
+                $arrDomain = AppDomain::spacegolfDomain();
+                break;
+            case AppDomain::FRUIT_STORE:
+                $arrDomain = AppDomain::fruitDomain();
+                break;
+            case AppDomain::HOME_HTP:
+                $arrDomain = AppDomain::htpDomain();
+                break;
+            case AppDomain::HOME_PAY:
+                $arrDomain = AppDomain::htpDomain();
+                break;
+            case AppDomain::BLOG:
+                $arrDomain = AppDomain::all();
+                break;
+            default:
+                $arrDomain = AppDomain::all();
+                break;
+        }
+
+
+        return $arrDomain;
+    }
+
+    /**
+     * Camel case to underscore string input
+     *
+     * @param $string
+     * @return string
+     */
+    public static function decamelize($string)
+    {
+        return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $string));
+    }
+}
